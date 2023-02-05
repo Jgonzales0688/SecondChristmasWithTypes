28adf351b28f9efa87ae36fb607f3d412ad9309d
Jessica <jgonzales0688@gmail.com>
2023-02-04 20:10:51 -0600

    Add project files.

---
+++ Christmas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondChristmasWithTypes
+{
+    public class Christmas
+    {
+        public string[] Presents;
+        public string Santa { get; set; }
+        public int TreeHeight { get; set; }
+
+        public enum Day { Monday, Tuesday, Wednesday, Thursday, Friday }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SecondChristmasWithTypes
+{
+    class Program
+    { 
+        static void Main(string[] args) 
+        {
+            var xmas = new Christmas();
+            var xmasDay = Christmas.Day.Thursday;
+
+            //TODO set Santa's nameto Kris Kringle
+            xmas.Santa = "Kris Kringle";
+
+            //TODO Insert 3 presents you would like for xmas. -must be strings
+            xmas.Presents = new string[3] { "Xbox", "Playstation", "Makeup" };
+
+            //TODO Set the TreeHeight to 10
+            xmas.TreeHeight = 10;
+
+            Console.WriteLine($"This year  christmas falls on {xmasDay}\n");
+            Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
+            Console.WriteLine("Here are the presents we would like:");
+
+            foreach (var present in xmas.Presents) 
+            {
+                Console.WriteLine($"     {present}");
+            }
+            Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
+
+        }
+    }
+}
